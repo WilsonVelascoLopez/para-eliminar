e6bf52cd669a011d63aebda1a02fab0025540646
WilsonVelascoLopez <wili@gmail.com>
2022-02-08 11:15:28 -0500

    commit 2

--- valor producto con ganancia.py
+++ valor producto con ganancia.py
@@ -1,5 +1,5 @@
 #programa para encontrar ladistancia recorrida por un movil en tiempo determonado y con una velocidad conocida.
-#autor: wilson velasco, mayo 11 de 2021.#
+#autor: wilson velasco, mayo 11 de 2021. revisado feb 8 de 2022#
 
 
 #entrada
