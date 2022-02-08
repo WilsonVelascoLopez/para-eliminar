a1b686aa0d14c33f7f7ba4940ffba13c6371fe12
WilsonVelascoLopez <wili@gmail.com>
2022-02-08 11:02:58 -0500

    commit 1

---
+++ valor producto con ganancia.py
@@ -0,0 +1,17 @@
+#programa para encontrar ladistancia recorrida por un movil en tiempo determonado y con una velocidad conocida.
+#autor: wilson velasco, mayo 11 de 2021.#
+
+
+#entrada
+controladorCiclo = True
+
+#proceso
+while (controladorCiclo):
+    numeroEntero = int(input("ingrese un numero entero:\n"))
+    if(numeroEntero < 10):
+        print("numero entero es: ", numeroEntero)
+        controladorCiclo = False
+
+
+
+
